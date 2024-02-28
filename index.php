06e2a37301d8090a14e2b3cdcc211b310e940af7
Naisyela <156977204+Naisyela@users.noreply.github.com>
2024-02-28 19:27:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>portofolio</title>
+    
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="home">
+    <nav>
+        <h2 class="logo">portofo<span>lio</span></h2>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#pengalaman">Pengalaman</a></li>
+            <li><a href="#skil">Skil</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        
+    </nav>
+    <div class="image">
+       
+    </div>
+    <div class="content">
+        <h4>Hallo Nama Saya</h4>
+        <h1>Naisyela Amelia Putri <spn></span></h1>
+        <h3>saya adalah siswi SMK Al-Ghifari</h3>
+        <a href="#"><i class="far fa-user">More About Me</i></a>
+    </div>
+</div>
+
+<section class="about">
+    <div class="about-me">
+        <img src="image/foto.jpg">
+        <div class="about-text">
+            <h2 id="about">About <span>Me</span></h2>
+            <h5>Saya adalah siswi <span>SMK Al-Ghifari</span></h5>
+            <p>Saya mengambil jurusan Rekayasa Perangkat Lunak, dan perkenalkan Nama saya Naisyela saya sering di panggil Nais dan Saya lahir di Bandung 16 April 2006 pada hari rabu ,saya juga mempunyai skill berwira usaha sejak saya  sekolah dasar saya sudah berdagang dibidang makanan dan cita-cita saya ingin menjadi pengusaha yang sukses amin... </p>
+            <a href="#" class="button">Contact Me</a>
+        </div>
+    </div>
+</section>
+
+<section class="about">
+    <div class="about-me">
+        <img src="image/foto.jpg" width="380px">
+        <div class="about-text">
+            <h2 id="pengalaman">Pengalaman<span></span></h2>
+            <h5>Praktik kerja lapang <span>DISPERINDAG</span></h5>
+            <p></p>
+            <a href="#" class="button">Contact Me</a>
+        </div>
+    </div>
+</section>
+
+<section class="skill">
+    <div class="title">
+        <h2 id="skil">skills<span></span></h2>
+    </div>
+     <div class="box">
+        <div class="card">
+        <i class="fas fa-bars"></i>
+        <h5>Front-End Developer</h5>
+        <div class="skill-bar">
+            <p>HTML</p>
+            <div class="skill-box">
+                <div class="html">70%</div>
+            </div>
+            <p>CSS</p>
+            <div class="skill-box">
+                <div class="css">60%</div>
+            </div>
+            <p>Javascript</p>
+            <div class="skill-box">
+                <div class="Javascript">60%</div>
+            </div>
+            <p>React js</p>
+            <div class="skill-box">
+                <div class="React">25%</div>
+            </div>
+        </div>
+        </div>
+        <div class="card">
+        <i class="fas fa-user"></i>
+        <h5>Back-End Developer</h5>
+        <div class="skill-bar">
+            <p>PHP</p>
+            <div class="skill-box">
+                <div class="PHP">70%</div>
+            </div>
+            <p>phyton</p>
+            <div class="skill-box">
+                <div class="phyton">10%</div>
+            </div>
+            <p>Mysql</p>
+            <div class="skill-box">
+                <div class="Mysql">75%</div>
+            </div>
+            <p>Firebase</p>
+            <div class="skill-box">
+                <div class="Firebase">0%</div>
+            </div>
+        </div>
+        </div>
+
+</section>
+
+<section class="Contact-me">
+    <div class="contact-text">
+        <h2 id="contact">Contact<span>Me</span></h2>
+        <div class="contact-info">
+            <div class="contact-box">
+                <ul>
+                    <li>Alamat:Kp.Tegalsari</li>
+                    <li>Email: Amelia Putri@gmail.com</li>
+                    <li>Telpon :083185576501</li>
+                </ul>
+                <!-- <div class="Contact">
+                    <h1>alamat</h1>
+                    <p>cikukuk</p>
+                </div>
+                <div class="Contact">
+                    <h4>Email</h4>
+                    <p>Salsabila pasa6@gmail.com</p>
+                </div>
+                <div class="Contact">
+                    <h4>Telp</h4>
+                    <p>(083129154763)</p>
+                </div>
+            </div>
+            </div>
+        </div>
+</section>
+
+<script src="http://kit.fontawesome,com/de8de52638.js" crossorigin="anonymous"></script>
+</body>
+</html>
